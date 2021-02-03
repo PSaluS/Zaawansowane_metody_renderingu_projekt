695f7c39272bd0854b3f50c9fc26cd5a1f4cf719
Pawel Salus <pluton123_14@o2.pl>
2021-02-03 20:58:30 +0100

    envTextures vs. true reflector mirror

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './style.scss';
 import * as THREE from 'three';
 import OrbitControls from 'three-orbitcontrols';
 import Stats from 'stats.js';
+import { Reflector } from '../node_modules/three/examples/jsm/objects/Reflector.js';
 
 const root = document.getElementById("root");
 const scene = new THREE.Scene();
@@ -10,6 +11,9 @@ const renderer = new THREE.WebGLRenderer();
 const controls = new OrbitControls(camera, renderer.domElement);
 const stats = new Stats();
 
+let jumpBall;
+let jumpTarget = 'down';
+
 const PointLight = new THREE.PointLight(0xffffff, 1.3, 20);
 
 const AmbientLight = new THREE.AmbientLight(0xffffff, 0.2);
@@ -17,11 +21,33 @@ const AmbientLight = new THREE.AmbientLight(0xffffff, 0.2);
 renderer.setSize(root.offsetWidth * 0.995, root.offsetHeight * 0.995);
 root.appendChild(renderer.domElement);
 
+function jump(up, down, step) {
+    
+    if(jumpTarget === 'down') {
+
+        if(jumpBall.position.y > down) {
+            jumpBall.position.y -= step;
+        }
+        else {
+            jumpTarget = 'up';
+        }
+    }
+    if(jumpTarget === 'up') {
+
+        if(jumpBall.position.y < up) {
+            jumpBall.position.y += step;
+        }
+        else {
+            jumpTarget = 'down';
+        }
+    }
+}
+
 function animate() {
 
     stats.begin();
 
-
+    jump(15,-15, 0.3);
 
     renderer.render(scene, camera);
     controls.update();
@@ -53,9 +79,16 @@ function init() {
     scene.add(AmbientLight);
     PointLight.position.set(0, 5.5, 0);
 
-    console.log(envTexture());
+    const jGeometry = new THREE.SphereGeometry(3,32,32);
+    const jMaterial = new THREE.MeshPhongMaterial({ color: 0xff3333 });
+    jumpBall = new THREE.Mesh(jGeometry, jMaterial);
+    scene.add(jumpBall);
+    jumpBall.position.set(10,0,10);
 
-    const geometry = new THREE.BoxGeometry(5,5,5);
+    // console.log(envTexture());
+
+    // const geometry = new THREE.BoxGeometry(5,5,5);
+    const geometry = new THREE.SphereGeometry(5,32,32);
     const material = new THREE.MeshBasicMaterial( { color: 0xffffff, envMap: envTexture() } );
     const box = new THREE.Mesh( geometry, material );
     scene.add( box );
@@ -68,7 +101,20 @@ function init() {
         'https://webglfundamentals.org/webgl/resources/images/computer-history-museum/neg-y.jpg',
         'https://webglfundamentals.org/webgl/resources/images/computer-history-museum/pos-z.jpg',
         'https://webglfundamentals.org/webgl/resources/images/computer-history-museum/neg-z.jpg'
-	] );
+    ] );
+    
+    const mirrorBack1 = new Reflector(new THREE.PlaneBufferGeometry(50, 50), {
+		color: new THREE.Color(0x7F7F7F),
+		textureWidth: window.innerWidth * window.devicePixelRatio,
+		textureHeight: window.innerHeight * window.devicePixelRatio,
+	});
+
+    mirrorBack1.position.y = -20;
+    mirrorBack1.rotateX(-Math.PI /2);
+	// mirrorBack1.position.z = -1
+    scene.add(mirrorBack1);
+    
+
 
 
     stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
